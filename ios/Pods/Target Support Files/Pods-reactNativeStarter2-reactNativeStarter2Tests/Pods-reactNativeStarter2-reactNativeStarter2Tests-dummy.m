621705d5b2d12c5509728fde4bbcaf5295d9f05c
Jonathan Zeru <jonathanzeru21@gmailcom>
2024-12-31 21:32:12 +0300

    first commit

---
+++ ios/Pods/Target Support Files/Pods-reactNativeStarter2-reactNativeStarter2Tests/Pods-reactNativeStarter2-reactNativeStarter2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeStarter2_reactNativeStarter2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeStarter2_reactNativeStarter2Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-reactNativeStarter2/Pods-reactNativeStarter2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeStarter2 : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeStarter2
+@end
